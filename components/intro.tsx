e1b19c59fcdc44e3a07a475f53dbd985bf4fb175
Md. Hasibur Rahman <57056025+TheHasibRahman@users.noreply.github.com>
2023-08-06 21:44:04 +0600

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -31,7 +31,7 @@ export default function Intro() {
             }}
           >
             <Image
-              src="https://raw.githubusercontent.com/TheHasibRahman/v2/master/src/images/me.jpg?token=GHSAT0AAAAAACF7KMPOBEZU7NQIUCMJN7COZGPXPSQ"
+              src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
               alt="Hasib portrait"
               width="192"
               height="192"
